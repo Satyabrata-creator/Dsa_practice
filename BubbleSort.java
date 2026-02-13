8389cae28a01c1a34d6c74ea7db5f9b5982e8780
Satyabrata-creator <satyabratam1970@gmail.com>
2026-02-13 08:57:54 +0530

    checking Conflict

--- BubbleSort.java
+++ BubbleSort.java
@@ -7,9 +7,9 @@ class BubbleSort{
             for(j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1])
                 {
-                    int temp=arr[j];
+                    int tmp=arr[j];
                     arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+                    arr[j+1]=tmp;
                 }
             }
         }
---
+++ q2.java
@@ -0,0 +1,3 @@
+public enum q2 {
+    sxslkopixjugsyfctgxsxslxncbalkmakm
+}
