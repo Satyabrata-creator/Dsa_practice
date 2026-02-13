f322ea4605a3f12bf50b30c02e1e737218a79b4d
Satyabrata-creator <satyabratam1970@gmail.com>
2026-02-13 09:05:26 +0530

    checking Conflict

--- BubbleSort.java
+++ BubbleSort.java
@@ -7,9 +7,9 @@ class BubbleSort{
             for(j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1])
                 {
-                    int tmp=arr[j];
+                    int temp=arr[j];
                     arr[j]=arr[j+1];
-                    arr[j+1]=tmp;
+                    arr[j+1]=temp;
                 }
             }
         }
