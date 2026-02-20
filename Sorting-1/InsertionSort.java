5f7c9ff8185377dbf5605c79b2bc29a1b26214cf
Satyabrata-creator <satyabratam1970@gmail.com>
2026-02-20 07:14:33 +0530

    InsetionSort Added

--- Sorting-1/InsertionSort.java
+++ Sorting-1/InsertionSort.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class InsertionSort {
-    
+    public static int[] insertionSort(int []arr){
+        for(int i=0;i<arr.length-1;i++){
+            int key=arr[i+1];
+            int j=i;
+            while( j>=0 && key<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+                arr[j+1]=key;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int a=sc.nextInt();
+        int []arr=new int[a];
+        System.out.print("Enter the elements into the array");
+        for(int i=0;i<a;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The sorted array is");
+        insertionSort(arr);
+        for(int i=0;i<a;i++){
+        System.out.println(arr[i]);
+        }
+    }
 }
