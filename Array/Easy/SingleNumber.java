dd024a446920e3574fe3e241e64fb01427dffb97
Satyabrata-creator <satyabratam1970@gmail.com>
2026-03-18 19:59:44 +0530

    single occurance of the number in the array

---
+++ Array/Easy/SingleNumber.java
@@ -0,0 +1,21 @@
+public class SingleNumber{
+    public static int singleNumber(int []nums){
+        for(int i=0;i<nums.length;i++){
+                int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]==nums[i]){
+                    count++;
+                }
+            }
+            if(count==1){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+         int a[]={1,1,2,2,3,3,4,4,5,9,5};
+        int k=singleNumber(a);
+        System.out.println(k);
+    }
+}
